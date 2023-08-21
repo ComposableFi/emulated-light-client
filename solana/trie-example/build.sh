293f79bc061e695d3827597fa55d8ad6ec3cae96
Michal Nazarewicz <mina86@mina86.com>
2023-08-21 14:43:02 +0200

    review

--- solana/trie-example/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -eu
-
-root=$(git rev-parse --show-toplevel)
-cd "$root"
-cargo build-sbf \
-      --manifest-path=solana/trie-example/Cargo.toml \
-      --sbf-out-dir=dist/trie-example
