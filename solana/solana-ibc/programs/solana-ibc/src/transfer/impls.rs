b5f13d85911605009f948e0d2f5e7de400e1546f
dhruvja <dhruv@iamsizzling.com>
2024-01-05 17:06:45 +0530

    remove unneccesary logging

--- solana/solana-ibc/programs/solana-ibc/src/transfer/impls.rs
+++ solana/solana-ibc/programs/solana-ibc/src/transfer/impls.rs
@@ -44,7 +44,6 @@ fn get_escrow_account(
         .unwrap_or((denom.as_bytes(), &b""[..]));
     let seeds = [port_id.as_bytes(), channel_id.as_bytes(), head, tail];
     let seeds = if tail.is_empty() { &seeds[..3] } else { &seeds[..] };
-    msg!("denom {:?} portid {:?} channelId {:?}", denom, port_id, channel_id);
     Pubkey::find_program_address(seeds, &crate::ID).0
 }
 
@@ -337,8 +336,6 @@ impl IbcStorage<'_, '_> {
             denom.rsplit_once('/').unwrap_or((&denom, &denom)).1;
         let escrow = get_escrow_account(port_id, channel_id, denom);
         
-        msg!("This is escrow account {:?}", escrow);
-        msg!("This is escrow account {:?}", accounts.escrow_account);
         accounts
             .escrow_account
             .as_ref()
