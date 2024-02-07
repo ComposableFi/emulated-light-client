bf46bedbf04e1ebc933a01abed535fb2d4484602
dhruvja <dhruv@iamsizzling.com>
2024-02-07 12:50:02 +0530

    added cli config

---
+++ validator/src/command.rs
@@ -0,0 +1,214 @@
+use std::{
+    fmt::{Debug, Display},
+    fs,
+    str::FromStr,
+};
+
+use anchor_client::solana_sdk::{signature::{read_keypair_file, Keypair}, signer::Signer};
+use clap::{arg, command, Args, Parser, Subcommand};
+use dialoguer::{theme::ColorfulTheme, Input};
+use log::LevelFilter;
+use serde::{Deserialize, Serialize};
+
+use crate::utils::{config_file, setup_logging};
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Config {
+    pub rpc_url: String,
+    pub ws_url: String,
+    pub program_id: String,
+    pub genesis_hash: String,
+    pub keypair: Vec<u8>,
+    pub log_level: String,
+}
+
+impl Display for Config {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let keypair = Keypair::from_bytes(&self.keypair).unwrap();
+        write!(f, "\nrpc_url: {}\nws_url: {}\nprogram_id: {}\ngenesis_hash: {}\nvalidator_public_key: {}\nlog_level: {}", self.rpc_url, self.ws_url, self.program_id, self.genesis_hash, keypair.pubkey().to_string(), self.log_level)
+    }
+}
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+#[command(arg_required_else_help(true))]
+#[command(next_line_help = true)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    /// Command to run the validator once the config has been set.
+    Run(RunArgs),
+    /// Command to run the validator for the first time where the config is set.
+    Init(InitArgs),
+}
+
+#[derive(Args, Debug)]
+struct RunArgs {
+    /// rpc url
+    #[arg(short, long)]
+    rpc_url: Option<String>,
+
+    /// websocket url
+    #[arg(short, long)]
+    ws_url: Option<String>,
+
+    /// program ID
+    #[arg(long)]
+    program_id: Option<String>,
+
+    /// genesis hash
+    #[arg(short, long)]
+    genesis_hash: Option<String>,
+
+    /// Private key
+    #[arg(long)]
+    keypair_path: Option<String>,
+
+    /// Log Level
+    #[arg(short, long)]
+    log_level: Option<LevelFilter>,
+}
+
+#[derive(Args, Debug)]
+struct InitArgs {
+    /// rpc url
+    #[arg(short, long)]
+    rpc_url: String,
+
+    /// websocket url
+    #[arg(short, long)]
+    ws_url: String,
+
+    /// program ID
+    #[arg(long)]
+    program_id: String,
+
+    /// genesis hash
+    #[arg(short, long)]
+    genesis_hash: String,
+
+    /// Private key
+    #[arg(long)]
+    keypair_path: String,
+
+    /// Log Level
+    #[arg(short, long)]
+    log_level: Option<LevelFilter>,
+}
+
+#[derive(Clone, Debug)]
+pub enum Values {
+    Yes,
+    No,
+}
+
+impl FromStr for Values {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        if s.eq_ignore_ascii_case("yes") {
+            Ok(Values::Yes)
+        } else if s.eq_ignore_ascii_case("no") {
+            Ok(Values::No)
+        } else {
+            Err(format!("Can not parse {}", s).into())
+        }
+    }
+}
+
+impl Display for Values {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+pub fn parse_config() -> Option<Config> {
+    let args = Cli::parse();
+    match args.command {
+        Commands::Run(cmd) => {
+            let config_file = config_file();
+            let config_data = fs::read_to_string(config_file).expect(
+                "Failed to read config file, make sure u run init command \
+                 before you try to run.",
+            );
+            let default_config: Config = toml::from_str(&config_data).unwrap();
+            let keypair = if cmd.keypair_path.is_some() {
+              let keypair = read_keypair_file(&cmd.keypair_path.unwrap()).expect("Unable to read keypair file");
+              keypair.to_bytes().to_vec()
+            } else {
+              default_config.keypair
+            };
+            let config = Config {
+                rpc_url: cmd.rpc_url.unwrap_or(default_config.rpc_url),
+                ws_url: cmd.ws_url.unwrap_or(default_config.ws_url),
+                program_id: cmd.program_id.unwrap_or(default_config.program_id),
+                genesis_hash: cmd
+                    .genesis_hash
+                    .unwrap_or(default_config.genesis_hash),
+                keypair,
+                log_level: cmd
+                    .log_level
+                    .unwrap_or(LevelFilter::Info)
+                    .to_string(),
+            };
+            setup_logging(&config.log_level);
+            Some(config)
+        }
+        Commands::Init(cmd) => {
+            let config_file = config_file();
+            let file = fs::metadata(config_file.clone());
+            setup_logging(
+                &cmd.log_level.unwrap_or(LevelFilter::Info).to_string(),
+            );
+            if file.is_ok() {
+                let value = prompt(
+                    "Do you really want to overwrite. Enter yes or no.",
+                    Some(Values::No),
+                );
+                if matches!(value, Values::No) {
+                    log::info!("Skipped overwriting the config file");
+                    return None;
+                }
+                log::info!("Overwriting config file");
+            }
+            let keypair = read_keypair_file(&cmd.keypair_path).expect("Unable to read keypair file");
+            let keypair = keypair.to_bytes().to_vec();
+            let config = Config {
+                rpc_url: cmd.rpc_url,
+                ws_url: cmd.ws_url,
+                program_id: cmd.program_id,
+                genesis_hash: cmd.genesis_hash,
+                keypair,
+                log_level: cmd
+                    .log_level
+                    .unwrap_or(LevelFilter::Info)
+                    .to_string(),
+            };
+            let toml_in_string = toml::to_string(&config).unwrap();
+            fs::write(config_file, toml_in_string).unwrap();
+            log::info!("New Config {}", config);
+            None
+        }
+    }
+}
+
+/// Prompt for user input with the ability to provide a default value
+fn prompt<T>(prompt: &str, default: Option<T>) -> T
+where
+    T: Clone + ToString + FromStr,
+    <T as FromStr>::Err: Debug + ToString,
+{
+    let theme = &ColorfulTheme::default();
+    let mut builder = Input::<T>::with_theme(theme);
+    builder.with_prompt(prompt);
+
+    if let Some(default) = default {
+        builder.default(default);
+    }
+
+    builder.interact_text().unwrap()
+}
--- validator/src/main.rs
+++ validator/src/main.rs
@@ -1,5 +1,3 @@
-use std::fs::File;
-use std::io::Read;
 use std::rc::Rc;
 use std::str::FromStr;
 
@@ -9,41 +7,28 @@ use anchor_client::solana_client::rpc_config::{
     RpcTransactionLogsFilter,
 };
 use anchor_client::solana_sdk::commitment_config::CommitmentConfig;
-use anchor_client::solana_sdk::ed25519_instruction::{
-    DATA_START, PUBKEY_SERIALIZED_SIZE, SIGNATURE_SERIALIZED_SIZE,
-};
 use anchor_client::solana_sdk::signature::Keypair;
 use anchor_client::solana_sdk::signer::Signer;
-use anchor_client::solana_sdk::{self};
 use anchor_client::{Client, Cluster};
-use anchor_lang::solana_program::instruction::Instruction;
 use anchor_lang::solana_program::pubkey::Pubkey;
-use base64::Engine;
-use bytemuck::bytes_of;
 use lib::hash::CryptoHash;
-use serde::Deserialize;
 use solana_ibc::{accounts, instruction};
 
-#[derive(Deserialize, Debug)]
-pub struct Config {
-    pub rpc_url: String,
-    pub ws_url: String,
-    pub program_id: String,
-    pub genesis_hash: String,
-    pub keypair: Vec<u8>,
-}
+mod command;
+mod utils;
+
 
 fn main() {
-    setup_logging();
+    let config = command::parse_config();
 
-    let mut file =
-        File::open("./validator/config.toml").expect("config file not found");
-    let mut contents = String::new();
-    file.read_to_string(&mut contents).unwrap();
-    let config: Config = toml::from_str(&contents).unwrap();
-    log::info!("Config: {:?}", config);
+    if config.is_none() {
+        return;
+    }
 
-    let validator = Rc::new(Keypair::from_bytes(&config.keypair).unwrap());
+    let config = config.unwrap();
+
+    let validator =
+        Rc::new(Keypair::from_bytes(&config.keypair).unwrap());
     let client = Client::new_with_options(
         Cluster::from_str(&config.rpc_url).expect("Invalid cluster"),
         validator.clone(),
@@ -79,7 +64,7 @@ fn main() {
             .recv()
             .unwrap_or_else(|err| panic!("{}", format!("Disconnected: {err}")));
 
-        let events = get_events_from_logs(logs.value.logs);
+        let events = utils::get_events_from_logs(logs.value.logs);
         if events.is_empty() {
             continue;
         }
@@ -97,7 +82,7 @@ fn main() {
         // Send the signature
         let tx = program
             .request()
-            .instruction(new_ed25519_instruction_with_signature(
+            .instruction(utils::new_ed25519_instruction_with_signature(
                 &validator.pubkey().to_bytes(),
                 signature.as_ref(),
                 fingerprint.as_slice(),
@@ -121,72 +106,5 @@ fn main() {
         if tx.is_ok() {
             log::info!("Block signed -> Transaction: {}", tx.unwrap());
         }
-    }
-}
-
-pub fn setup_logging() {
-    env_logger::builder().format_module_path(false).init();
-}
-
-fn get_events_from_logs(
-    logs: Vec<String>,
-) -> Vec<solana_ibc::events::NewBlock<'static>> {
-    logs.iter()
-        .filter_map(|log| log.strip_prefix("Program data: "))
-        .filter_map(|event| {
-            let decoded_event =
-                base64::prelude::BASE64_STANDARD.decode(event).unwrap();
-            let decoded_event: solana_ibc::events::Event =
-                borsh::BorshDeserialize::try_from_slice(&decoded_event)
-                    .unwrap();
-            match decoded_event {
-                solana_ibc::events::Event::NewBlock(e) => Some(e),
-                _ => {
-                    // println!("This is other event");
-                    None
-                }
-            }
-        })
-        .collect()
-}
-
-/// Solana sdk only accepts a keypair to form ed25519 instruction.
-/// Until they implement a method which accepts a pubkey and signature instead of keypair
-/// we have to use the below method instead.
-///
-/// Reference: https://github.com/solana-labs/solana/pull/32806
-pub fn new_ed25519_instruction_with_signature(
-    pubkey: &[u8],
-    signature: &[u8],
-    message: &[u8],
-) -> Instruction {
-    assert_eq!(pubkey.len(), PUBKEY_SERIALIZED_SIZE);
-    assert_eq!(signature.len(), SIGNATURE_SERIALIZED_SIZE);
-
-    let num_signatures: u8 = 1;
-    let public_key_offset = DATA_START;
-    let signature_offset =
-        public_key_offset.saturating_add(PUBKEY_SERIALIZED_SIZE);
-    let message_data_offset =
-        signature_offset.saturating_add(SIGNATURE_SERIALIZED_SIZE);
-
-    let offsets = solana_ibc::ed25519::SignatureOffsets {
-        signature_offset: signature_offset as u16,
-        signature_instruction_index: u16::MAX,
-        public_key_offset: public_key_offset as u16,
-        public_key_instruction_index: u16::MAX,
-        message_data_offset: message_data_offset as u16,
-        message_data_size: message.len() as u16,
-        message_instruction_index: u16::MAX,
-    };
-
-    let instruction =
-        [&[num_signatures, 0], bytes_of(&offsets), pubkey, signature, message]
-            .concat();
-
-    Instruction {
-        program_id: solana_sdk::ed25519_program::id(),
-        accounts: vec![],
-        data: instruction,
     }
 }
