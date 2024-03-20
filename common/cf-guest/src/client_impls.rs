a3c1173bd3ea2fc92039e54ac916e9b15aff96b4
Dhruv D Jain <dhruv@iamsizzling.com>
2024-03-20 19:03:52 -0300

    solana-ibc: update revision number of height to 1 (#253)
    Updating revision number of height to 1 from 0 since while decoding, the
    revision number field which is omitted while encoding ( since its 0 )
    fails when decoding because of missing field. It could be fixed by
    having a default value in case of missing field but since it would
    require upgrading of cosmos node, we can just use revision number as 1.

--- common/cf-guest/src/client_impls.rs
+++ common/cf-guest/src/client_impls.rs
@@ -77,7 +77,7 @@ impl<PK: PubKey> ibc::ClientStateCommon for ClientState<PK> {
     }
 
     fn latest_height(&self) -> ibc::Height {
-        ibc::Height::new(0, self.latest_height.into()).unwrap()
+        ibc::Height::new(1, self.latest_height.into()).unwrap()
     }
 
     fn validate_proof_height(&self, proof_height: ibc::Height) -> Result {
@@ -187,7 +187,7 @@ where
         let header = crate::proto::Header::try_from(header)?;
         let header = crate::Header::<PK>::try_from(header)?;
         let header_height =
-            ibc::Height::new(0, header.block_header.block_height.into())?;
+            ibc::Height::new(1, header.block_header.block_height.into())?;
 
         let (host_timestamp, host_height) = CommonContext::host_metadata(ctx)?;
         self.prune_oldest_consensus_state(ctx, client_id, host_timestamp)?;
@@ -275,7 +275,7 @@ where
             return Ok(ibc::Status::Frozen);
         }
 
-        let height = ibc::Height::new(0, self.latest_height.into())?;
+        let height = ibc::Height::new(1, self.latest_height.into())?;
         let consensus = CommonContext::consensus_state(ctx, client_id, height)
             .and_then(|state| state.try_into().map_err(error));
         let consensus = match consensus {
