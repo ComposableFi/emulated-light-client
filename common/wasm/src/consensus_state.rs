78f0c6ad176ac00fff445a388edb4828924865d2
Dhruv D Jain <dhruv@iamsizzling.com>
2024-03-30 00:55:31 -0300

    solana-ibc: add wasm consensus and client state (#255)
    The client and consensus state stored on the host are in wrapped in
    wasm. So we need to add a new wasm client state and consensus state type
    so that we can wrap the and unwrap the states.
    
    The client state is available in `ibc-rs` so we dont need to implement
    it ourselves. But the consensus state which is available in `ibc-rs`
    doesnt have `timestamp` field which the consensus state in relayer has.
    So we need to implement the protobuf for consensus state ourselves. This
    is implemented in the wasm crate.
    
    ---------
    
    Co-authored-by: Michal Nazarewicz <mina86@mina86.com>

---
+++ common/wasm/src/consensus_state.rs
@@ -0,0 +1,67 @@
+use ibc_primitives::proto::Any;
+use ibc_proto::Protobuf;
+
+use crate::proto;
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct ConsensusState {
+    pub data: Vec<u8>,
+    pub timestamp_ns: u64,
+}
+
+impl ConsensusState {
+    pub fn new(data: Vec<u8>, timestamp_ns: u64) -> Self {
+        Self { data, timestamp_ns }
+    }
+}
+
+impl ibc_core_client_context::consensus_state::ConsensusState
+    for ConsensusState
+{
+    fn root(&self) -> &ibc_core_commitment_types::commitment::CommitmentRoot {
+        todo!()
+    }
+
+    fn timestamp(&self) -> ibc_primitives::Timestamp {
+        ibc_primitives::Timestamp::from_nanoseconds(self.timestamp_ns).unwrap()
+    }
+
+    fn encode_vec(self) -> alloc::vec::Vec<u8> {
+        <Self as Protobuf<Any>>::encode_vec(self)
+    }
+}
+
+impl Protobuf<Any> for ConsensusState {}
+
+impl From<ConsensusState> for proto::ConsensusState {
+    fn from(state: ConsensusState) -> Self {
+        Self { data: state.data, timestamp_ns: state.timestamp_ns }
+    }
+}
+
+impl From<&ConsensusState> for proto::ConsensusState {
+    fn from(state: &ConsensusState) -> Self { Self::from(state.clone()) }
+}
+
+impl TryFrom<proto::ConsensusState> for ConsensusState {
+    type Error = proto::BadMessage;
+    fn try_from(msg: proto::ConsensusState) -> Result<Self, Self::Error> {
+        Ok(ConsensusState { data: msg.data, timestamp_ns: msg.timestamp_ns })
+    }
+}
+
+impl TryFrom<&proto::ConsensusState> for ConsensusState {
+    type Error = proto::BadMessage;
+    fn try_from(msg: &proto::ConsensusState) -> Result<Self, Self::Error> {
+        Ok(ConsensusState {
+            data: <Vec<u8> as Clone>::clone(&msg.data),
+            timestamp_ns: msg.timestamp_ns,
+        })
+    }
+}
+
+super::any_convert! {
+  proto::ConsensusState,
+  ConsensusState,
+  obj: ConsensusState::new(lib::hash::CryptoHash::test(42).to_vec(), 1),
+}
