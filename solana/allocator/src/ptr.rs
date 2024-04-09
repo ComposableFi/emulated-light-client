60fa0b91cccaa0dd97496fa78b2e3bb47f90b541
Michal Nazarewicz <mina86@mina86.com>
2024-04-09 18:01:25 +0200

    solana-allocator: ensure proper alignment when resizing (#282)

--- solana/allocator/src/ptr.rs
+++ solana/allocator/src/ptr.rs
@@ -2,7 +2,6 @@
 
 /// Creates a new pointer with the given address.
 // TODO(mina86): Use ptr.with_addr once strict_provenance stabilises.
-#[inline]
 pub(super) fn with_addr(ptr: *mut u8, addr: usize) -> *mut u8 {
     ptr.wrapping_add(addr.wrapping_sub(ptr as usize))
 }
@@ -10,7 +9,6 @@ pub(super) fn with_addr(ptr: *mut u8, addr: usize) -> *mut u8 {
 /// Aligns pointer to given alignment which must be a power of two.
 ///
 /// If `align` isn’t a power of two, result is unspecified.
-#[inline]
 pub(super) fn align(ptr: *mut u8, align: usize) -> *mut u8 {
     let mask = align - 1;
     debug_assert!(align != 0 && align & mask == 0);
@@ -19,13 +17,41 @@ pub(super) fn align(ptr: *mut u8, align: usize) -> *mut u8 {
 }
 
 /// Returns end address of given object.
-#[inline]
 pub(super) fn end_addr_of_val<T: Sized>(obj: &T) -> usize {
     (obj as *const T).wrapping_add(1) as usize
 }
 
 /// Returns a range of pointers of given size.
-#[inline]
 pub(super) fn range(start: *mut u8, size: usize) -> core::ops::Range<*mut u8> {
     start..start.wrapping_add(size)
+}
+
+
+/// Copies `size` bytes from `src` to `dst`.
+///
+/// # Safety
+///
+/// Caller must guarantees all of the conditions required by
+/// [`core::ptr::copy_nonoverlapping`].
+pub(super) unsafe fn memcpy(dst: *mut u8, src: *const u8, size: usize) {
+    if cfg!(debug_assertions) {
+        assert_no_overlap(dst, size, src, size);
+    }
+    // SAFETY: Caller guarantees all necessary conditions.
+    unsafe { core::ptr::copy_nonoverlapping(src, dst, size) }
+}
+
+#[track_caller]
+pub(super) fn assert_no_overlap(
+    a: *const u8,
+    a_size: usize,
+    b: *const u8,
+    b_size: usize,
+) {
+    let a = range(a as *mut u8, a_size);
+    let b = range(b as *mut u8, b_size);
+    assert!(
+        !a.contains(&b.start) && !a.contains(&b.end),
+        "{a:?} and {b:?} overlap",
+    )
 }
