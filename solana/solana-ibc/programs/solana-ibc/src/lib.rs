eecd9f77017015923d83d71a13529ec06ef16ddd
dhruvja <dhruv@iamsizzling.com>
2024-04-10 14:08:01 -0300

    update program id of solana-ibc

--- solana/solana-ibc/programs/solana-ibc/src/lib.rs
+++ solana/solana-ibc/programs/solana-ibc/src/lib.rs
@@ -23,7 +23,7 @@ pub const SOLANA_IBC_STORAGE_SEED: &[u8] = b"private";
 pub const TRIE_SEED: &[u8] = b"trie";
 pub const MINT_ESCROW_SEED: &[u8] = b"mint_escrow";
 
-declare_id!("9fd7GDygnAmHhXDVWgzsfR6kSRvwkxVnsY8SaSpSH4SX");
+declare_id!("2HLLVco5HvwWriNbUhmVwA2pCetRkpgrqwnjcsZdyTKT");
 
 mod allocator;
 pub mod chain;
