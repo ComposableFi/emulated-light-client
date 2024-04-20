55598f4c10acf34441187c596a7a7981ecaace20
dhruvja <dhruv@iamsizzling.com>
2024-04-20 15:39:31 +0400

    define schema and instruction set for calling send transfer from solana ibc

---
+++ solana/solana-ibc/tests/schema.ts
@@ -0,0 +1,46 @@
+import { BorshSchema, Unit } from "borsher";
+
+const packetDataSchema = BorshSchema.Struct({
+  token: BorshSchema.Struct({
+    denom: BorshSchema.Struct({
+      trace_path: BorshSchema.Vec(
+        BorshSchema.Struct({
+          port_id: BorshSchema.String,
+          channel_id: BorshSchema.String,
+        })
+      ),
+      base_denom: BorshSchema.String,
+    }),
+    amount: BorshSchema.Array(BorshSchema.u8, 32),
+  }),
+  sender: BorshSchema.String,
+  receiver: BorshSchema.String,
+  memo: BorshSchema.String,
+});
+
+const timeoutHeightSchema = BorshSchema.Enum({
+  Never: BorshSchema.Unit,
+  At: BorshSchema.Struct({
+    revision_number: BorshSchema.u64,
+    revision_height: BorshSchema.u64,
+  }),
+});
+const timeoutTimestampSchema = BorshSchema.Struct({
+  time: BorshSchema.u64,
+});
+
+export const msgTransferSchema = BorshSchema.Struct({
+  port_id_on_a: BorshSchema.String,
+  chan_id_on_a: BorshSchema.String,
+  packet_data: packetDataSchema,
+  timeout_height_on_b: timeoutHeightSchema,
+  timeout_timestamp_on_b: timeoutTimestampSchema,
+});
+
+export const instructionSchema = BorshSchema.Struct({
+  discriminator: BorshSchema.Array(BorshSchema.u8, 8),
+  port_id: BorshSchema.String,
+  channel_id: BorshSchema.String,
+  hashed_base_denom: BorshSchema.Array(BorshSchema.u8, 32),
+  msg: msgTransferSchema,
+});
---
+++ solana/solana-ibc/tests/utils.ts
@@ -0,0 +1,66 @@
+import * as anchor from "@coral-xyz/anchor";
+import * as spl from "@solana/spl-token";
+import { BorshSchema, borshSerialize, borshDeserialize, Unit } from "borsher";
+import { hash } from "@coral-xyz/anchor/dist/cjs/utils/sha256";
+import {
+  ComputeBudgetInstruction,
+  ComputeBudgetProgram,
+  Connection,
+  SystemProgram,
+  Transaction,
+  TransactionInstruction,
+  sendAndConfirmTransaction,
+} from "@solana/web3.js";
+import bs58 from "bs58";
+import { solanaIbcProgramId } from "./constants";
+
+export function getInt64Bytes(x: number) {
+  let y = Math.floor(x / 2 ** 32);
+  return [y, y << 8, y << 16, y << 24, x, x << 8, x << 16, x << 24].map(
+    (z) => z >>> 24
+  );
+}
+
+export function hexToBytes(hex: string) {
+  let bytes = [];
+  for (let c = 0; c < hex.length; c += 2)
+    bytes.push(parseInt(hex.substr(c, 2), 16));
+  return bytes;
+}
+
+export const getGuestChainAccounts = (portId: string, channelId: string, hashedDenom: number[]) => {
+  const [guestChainPDA, guestChainBump] =
+    anchor.web3.PublicKey.findProgramAddressSync(
+      [Buffer.from("chain")],
+      solanaIbcProgramId
+    );
+
+  const [triePDA, trieBump] = anchor.web3.PublicKey.findProgramAddressSync(
+    [Buffer.from("trie")],
+    solanaIbcProgramId
+  );
+
+  const [mintAuthorityPDA, mintAuthorityBump] =
+    anchor.web3.PublicKey.findProgramAddressSync(
+      [Buffer.from("mint_escrow")],
+      solanaIbcProgramId
+    );
+
+  const [ibcStoragePDA, ibcStorageBump] =
+    anchor.web3.PublicKey.findProgramAddressSync(
+      [Buffer.from("private")],
+      solanaIbcProgramId
+    );
+
+    const [escrowAccountPDA, escrowAccountBump] =
+    anchor.web3.PublicKey.findProgramAddressSync(
+      [
+        Buffer.from(portId),
+        Buffer.from(channelId),
+        Buffer.from(hashedDenom),
+      ],
+      solanaIbcProgramId
+    );
+
+  return { guestChainPDA, triePDA, ibcStoragePDA, mintAuthorityPDA, escrowAccountPDA };
+};
