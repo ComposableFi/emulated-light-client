8871acb8db4ef00ab0ee34454b68cbb274fc41a1
Michal Nazarewicz <mina86@mina86.com>
2024-04-23 17:19:13 +0200

    solana-ibc: remove unnecessary associated_token_program accounts

--- solana/solana-ibc/programs/solana-ibc/src/lib.rs
+++ solana/solana-ibc/programs/solana-ibc/src/lib.rs
@@ -492,7 +492,6 @@ pub struct InitMint<'info> {
               bump, mint::decimals = 6, mint::authority = mint_authority)]
     token_mint: Account<'info, Mint>,
 
-    associated_token_program: Program<'info, AssociatedToken>,
     token_program: Program<'info, Token>,
     system_program: Program<'info, System>,
 }
@@ -618,7 +617,6 @@ pub struct SendTransfer<'info> {
     #[account(mut)]
     receiver_token_account: Option<Box<Account<'info, TokenAccount>>>,
 
-    associated_token_program: Option<Program<'info, AssociatedToken>>,
     token_program: Option<Program<'info, Token>>,
     system_program: Program<'info, System>,
 }
