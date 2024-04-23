79d9a3e08f5edb321f4a3160c115521d3bbc6285
Markkus Millend <mm@liangroup.io>
2024-04-23 16:17:20 +0200

    Tag Docker image to a specific git tag or fallback to commit hash

--- docker/build_docker.sh
+++ docker/build_docker.sh
@@ -2,7 +2,8 @@
 set -euo pipefail
 
 image="composable/emulated-light-client"
+tag="$(git describe --tags --exact-match HEAD 2>/dev/null || git rev-parse --short HEAD)"
 
 docker build \
     -f ./docker/Dockerfile \
-    -t "${image}:latest" .
+    -t "${image}:${tag}" .
