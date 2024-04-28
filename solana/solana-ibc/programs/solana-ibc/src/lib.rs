82d4ac153f3009af491f0e0b945be4cd96d391be
dhruvja <dhruv@iamsizzling.com>
2024-04-28 06:55:46 +0530

    fmt

--- solana/solana-ibc/programs/solana-ibc/src/lib.rs
+++ solana/solana-ibc/programs/solana-ibc/src/lib.rs
@@ -29,7 +29,7 @@ pub const FEE_SEED: &[u8] = b"fee";
 
 pub const FEE_AMOUNT_IN_LAMPORTS: u64 =
     solana_program::native_token::LAMPORTS_PER_SOL / 100;
-pub const REFUND_FEE_AMOUNT_IN_LAMPORTS: u64 = 
+pub const REFUND_FEE_AMOUNT_IN_LAMPORTS: u64 =
     solana_program::native_token::LAMPORTS_PER_SOL / 100;
 pub const MINIMUM_FEE_TO_COLLECT: u64 =
     solana_program::native_token::LAMPORTS_PER_SOL;
