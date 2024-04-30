85b4d1ade9478d476a27b039b622ea2817a96beb
Michal Nazarewicz <mina86@mina86.com>
2024-04-30 21:30:37 +0200

    solana-ibc: limit number of stored consensus states (#313)

--- solana/solana-ibc/programs/solana-ibc/src/client_state.rs
+++ solana/solana-ibc/programs/solana-ibc/src/client_state.rs
@@ -189,30 +189,18 @@ impl ibc::tm::CommonContext for IbcStorage<'_, '_> {
         ibc::ValidationContext::consensus_state(self, client_cons_state_path)
     }
 
-    /// Returns **only the earliest** consensus height.
+    /// Returns an empty list.
     ///
     /// This method is used by Tendermint light client to prune old states.
-    /// Unfortunately, with Solana’s strict compute units limits, we end up
-    /// failing transaction if the light client tries to delete too many old
-    /// states.  Because of that, we hack this method to return only the
-    /// easiest height rather than all of them.  This limit the light client to
-    /// delete only one state at a time.
+    /// However, we are limiting number of consensus states we’re keeping in
+    /// store_consensus_state_and_metadata method, which makes it unnecessary
+    /// for the light client to perform the pruning.  Because of that, this
+    /// method returns an empty list.
     fn consensus_state_heights(
         &self,
-        client_id: &ibc::ClientId,
+        _client_id: &ibc::ClientId,
     ) -> Result<Vec<ibc::Height>, ibc::ContextError> {
-        let height = self
-            .borrow()
-            .private
-            .client(client_id)?
-            .consensus_states
-            .keys()
-            .copied()
-            .min();
-        match height {
-            Some(height) => Ok(alloc::vec![height]),
-            None => Ok(Vec::new()),
-        }
+        Ok(Vec::new())
     }
 
     fn host_timestamp(&self) -> Result<ibc::Timestamp, ibc::ContextError> {
@@ -309,18 +297,18 @@ impl cf_guest::CommonContext<sigverify::ed25519::PubKey>
         self.delete_consensus_state_impl(client_id, height)
     }
 
+    /// Returns `None`.
+    ///
+    /// This method is used by the light client to prune old states.  However,
+    /// we are limiting number of consensus states we’re keeping in
+    /// store_consensus_state_and_metadata method, which makes it unnecessary
+    /// for the light client to perform the pruning.  Because of that, this
+    /// method returns `None`.
     fn earliest_consensus_state(
         &self,
-        client_id: &ibc::ClientId,
+        _client_id: &ibc::ClientId,
     ) -> Result<Option<(ibc::Height, Self::AnyConsensusState)>> {
-        self.borrow()
-            .private
-            .client(client_id)?
-            .consensus_states
-            .iter()
-            .min_by(|(ref a, _), (ref b, _)| a.cmp(b))
-            .map(|(height, state)| state.state().map(|state| (*height, state)))
-            .transpose()
+        Ok(None)
     }
 }
 
