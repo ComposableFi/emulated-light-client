be2e588c3b8ce85e9983f1873e8bc2962cc3629d
dhruvja <dhruv@iamsizzling.com>
2024-06-16 15:57:43 -0400

    added restaking-v2 program to config

--- solana/restaking-v2/tests/constants.ts
+++
@@ -1,7 +0,0 @@
-export const restakingProgramId = "8n3FHwYxFgQCQc2FNFkwDUf9mcqupxXcCvgfHbApMLv3";
-export const guestChainProgramId =
-  "2HLLVco5HvwWriNbUhmVwA2pCetRkpgrqwnjcsZdyTKT";
-export const initialMintAmount = 100000000;
-export const depositAmount = 4000;
-export const boundingPeriod = 5; // seconds
-export const testSeed = "abcdefg2";
