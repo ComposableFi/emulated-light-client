4030bac64bee7168c48b327c89e350757ded17bd
Michal Nazarewicz <mina86@mina86.com>
2024-08-29 19:09:11 +0200

    cf-solana: add no-blake3-syscall feature disabling Solana blake3 builtin
    The sol_blake3 function is not currently enabled on Solana mainnet¹.
    Add a no-blake3-syscall Cargo feature to cf-solana which disables the
    use of the builtin and calculates the hash through blake3 crate.  This
    is slower, but hopefully we’ll be under compute units limit.
    
    ¹ https://github.com/anza-xyz/agave/wiki/Feature-Gate-Activation-Schedule#features-are-blocked
      https://github.com/solana-labs/solana/issues/27099

--- common/cf-solana/src/blake3.rs
+++ common/cf-solana/src/blake3.rs
@@ -2,17 +2,19 @@ pub use ::blake3::Hasher;
 
 use crate::types::Hash;
 
+const CONSIDER_SOL: bool =
+    !cfg!(feature = "no-blake3-syscall") && cfg!(target_os = "solana-program");
+const HAS_SOL: bool =
+    cfg!(feature = "solana-program") || cfg!(feature = "solana-program-2");
+const USE_SOL: bool = CONSIDER_SOL && HAS_SOL;
+
 /// Calculates Blake3 hash of given byte slice.
 ///
 /// When `solana-program` or `solana-program-2` feature is enabled and
 /// building a solana program, this is using Solana’s `sol_blake3` syscall.
 /// Otherwise, the calculation is done by `blake3` crate.
-#[allow(dead_code)]
 pub fn hash(bytes: &[u8]) -> Hash {
-    if cfg!(target_os = "solana-program") &&
-        (cfg!(feature = "solana-program") ||
-            cfg!(feature = "solana-program-2"))
-    {
+    if USE_SOL {
         hashv(&[bytes])
     } else {
         Hash(::blake3::hash(bytes).into())
@@ -24,23 +26,18 @@ pub fn hash(bytes: &[u8]) -> Hash {
 /// When `solana` or `solana2` feature is enabled and building a Solana
 /// program, this is using Solana’s `sol_blake3` syscall.  Otherwise, the
 /// calculation is done by `blake3` crate.
-#[allow(dead_code)]
+#[allow(unreachable_code)]
 pub fn hashv(slices: &[&[u8]]) -> Hash {
-    #[cfg(all(target_os = "solana-program", feature = "solana-program-2"))]
-    return Hash(solana_program_2::blake3::hashv(slices).0);
-    #[cfg(all(
-        target_os = "solana-program",
-        feature = "solana-program",
-        not(feature = "solana-program-2")
-    ))]
-    return Hash(solana_program::blake3::hashv(slices).0);
-
-    #[allow(dead_code)]
-    {
-        let mut hasher = Hasher::default();
-        for bytes in slices {
-            hasher.update(bytes);
-        }
-        hasher.finalize().into()
+    if USE_SOL {
+        #[cfg(feature = "solana-program-2")]
+        return Hash(solana_program_2::blake3::hashv(slices).0);
+        #[cfg(feature = "solana-program")]
+        return Hash(solana_program::blake3::hashv(slices).0);
     }
+
+    let mut hasher = Hasher::default();
+    for bytes in slices {
+        hasher.update(bytes);
+    }
+    hasher.finalize().into()
 }
